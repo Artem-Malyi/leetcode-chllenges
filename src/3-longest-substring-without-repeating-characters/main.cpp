5d86b23a840dcf144a9a5e39fc75ca35bad3c4ac
Artem Malyi <artem.malyi@gmail.com>
2020-08-11 00:56:22 +0300

    challenge 3: longest substring without repeating characters

---
+++ src/3-longest-substring-without-repeating-characters/main.cpp
@@ -0,0 +1,39 @@
+#include <Windows.h>
+
+#include <cassert>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+int lengthOfLongestSubstring(string s) {
+    int maxLength = 0, windowStart = 0;
+    unordered_map<char, int> freq;
+    for (int windowEnd = 0; windowEnd < s.size(); ++windowEnd) {
+        ++freq[s[windowEnd]];
+        while (freq[s[windowEnd]] > 1) {
+            --freq[s[windowStart]];
+            if (0 == freq[s[windowStart]])
+                freq.erase(freq[s[windowStart]]);
+            ++windowStart;
+        }
+        maxLength = max(maxLength, windowEnd - windowStart + 1);
+    }
+    return maxLength;
+}
+
+void testLengthOfLongestSubstring() {
+    assert(3 == lengthOfLongestSubstring("abcabcbb"));
+    assert(1 == lengthOfLongestSubstring("bbbbb"));
+    assert(3 == lengthOfLongestSubstring("pwwkew"));
+    assert(10 == lengthOfLongestSubstring("zxlkcvjhslkrhgoshufglaish dflkajhsdlkfjhalskjdhflakjshdf"));
+}
+
+int main() {
+
+    testLengthOfLongestSubstring();
+
+	return 0;
+}
