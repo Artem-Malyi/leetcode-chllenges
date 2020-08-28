fb945a6083ad4e8f8ec8f33dfa9ce6ca6730d1fd
Artem Malyi <artem.malyi@gmail.com>
2020-08-28 19:45:57 +0300

    challenge 76: minimum window substring

---
+++ src/76-minimum-window-substring/main.cpp
@@ -0,0 +1,65 @@
+#include <Windows.h>
+
+#include <cassert>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+#pragma warning(disable: 4267)
+
+string minWindow(string s, string t) {
+    // windowStart:          v
+    //              adobecodebanc , abc -> banc
+    // windowEnd:                ^
+
+    unordered_map<char, int> freq;
+    for (auto c : t)
+        ++freq[c]; // a:0, b:1, c:0
+
+    int windowStart = 0, matched = 0, minLength = s.size() + 1, subStrStart = 0;
+    // try to extend the range [windowStart, windowEnd]
+    for (int windowEnd = 0; windowEnd < s.size(); windowEnd++) { // 13
+        char rightChar = s[windowEnd]; // c
+        if (freq.find(rightChar) != freq.end()) { // true
+            --freq[rightChar];
+            if (freq[rightChar] >= 0) // count every matching of a character
+                ++matched; // 2
+        }
+
+        // shrink the window if we can, finish as soon as we remove a matched character
+        while (matched == t.size()) { // 2 == 3
+            if (minLength > windowEnd - windowStart + 1) { // 5 > 4
+                minLength = windowEnd - windowStart + 1; // 4
+                subStrStart = windowStart; // 9
+            }
+
+            char leftChar = s[windowStart++]; // b, 10
+            if (freq.find(leftChar) != freq.end()) { // false
+                // note that we could have redundant matching characters, therefore we'll decrement the
+                // matched count only when a useful occurrence of a matched character is going out of the
+                // window
+                if (freq[leftChar] == 0) { // 0 == 0
+                    matched--; // 2
+                }
+                freq[leftChar]++; // b:1
+            }
+        }
+    }
+
+    return minLength > s.size() ? "" : s.substr(subStrStart, minLength); // banc
+}
+
+void testMinWindow() {
+    assert("banc" == minWindow("adobecodebanc", "abc"));
+    assert("" == minWindow("alskjdfhalksjdhfalkjshdlfkjahsdf", "xcvb"));
+}
+
+int main() {
+
+    testMinWindow();
+
+	return 0;
+}
--- src/challenge1/main.cpp
+++ src/challenge1/main.cpp
@@ -3,6 +3,7 @@
 #include <cassert>
 #include <iostream>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 using namespace std;
