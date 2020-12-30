ae8b56acd11e7d15bc8f50d31a8d07978371879c
Artem Malyi <artem.malyi@gmail.com>
2020-12-30 18:59:31 +0200

    updated docs for the rest of the problems

--- src/5-longest-palindromic-substring/main.cpp
+++ src/5-longest-palindromic-substring/main.cpp
@@ -1,3 +1,29 @@
+// https://leetcode.com/problems/longest-palindromic-substring/
+// 5. Longest Palindromic Substring
+
+// Given a string s, return the longest palindromic substring in s.
+//
+// Example 1:
+//   Input: s = "babad"
+//   Output : "bab"
+//   Note : "aba" is also a valid answer.
+// Example 2:
+//   Input : s = "cbbd"
+//   Output : "bb"
+// Example 3:
+//   Input : s = "a"
+//   Output : "a"
+// Example 4:
+//   Input : s = "ac"
+//   Output : "a"
+// Constraints:
+//    1 <= s.length <= 1000
+//    s consist of only digits and English letters(lower - caseand /or upper - case)
+
+// Time:
+// Space:
+// Pattern:
+
 #include <Windows.h>
 
 #include <cassert>
