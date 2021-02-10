90004672f5384d12666399cda17861b74d555e68
Artem Malyi <artem.malyi@gmail.com>
2021-02-10 08:13:57 +0200

    missing comments

--- src/199-binary-tree-right-side-view/main.cpp
+++ src/199-binary-tree-right-side-view/main.cpp
@@ -1,7 +1,8 @@
 // https://leetcode.com/problems/binary-tree-right-side-view/
 // 199. Binary Tree Right Side View
 
-// problem description with examples
+// Given a binary tree, imagine yourself standing on the right side of it, 
+// return the values of the nodes you can see ordered from top to bottom.
 // Example 1:
 //              1 <--
 //            /   \
